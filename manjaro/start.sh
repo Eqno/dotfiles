1abcfe67afb11e87c71b6758ec964a084d64c04e
范若筠 <2367542784@qq.com>
2021-09-05 17:14:47 +0800

    update config

--- manjaro/start.sh
+++ manjaro/start.sh
@@ -4,6 +4,7 @@ sudo cp ./profile /etc/
 
 sudo cp ./pacman.conf /etc/
 sudo cp ./mirrorlist /etc/pacman.d/
+cp ./.Xresources ~/.Xresources 
 
 sudo pacman -S vim neovim python3 python2 xsel nodejs clash
 cp ./update-clash ~/.local/bin/
@@ -31,4 +32,9 @@ curl https://sh.rustup.rs -sSf | sh
 curl -sL install-node.now.sh | sh
 curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
 
-
+# 桌面壁纸：/home/eqnoxx/Pictures/Wallpapers/background.jpg
+# 登陆界面壁纸：sudo vim /etc/lightdm/slick-greeter.conf
+# 系统语言：sudo vim /etc/default/locale
+# 窗口透明软件：sudo pacman -S picom
+# 终端软件：sudo pacman -S urxvt
+# 终端配置文件：vim .Xresources
